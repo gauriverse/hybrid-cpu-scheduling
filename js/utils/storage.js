862b5477ab15a7be9ac1e13c2310dec5203a8a28
Gauri <potdargau.14@gmail.com>
2026-02-18 17:50:37 +0530

    add storage utility for simulator state

---
+++ js/utils/storage.js
@@ -0,0 +1,66 @@
+// Simulator State Management
+
+let processes = [];
+let selectedAlgorithm = 'fcfs';
+let processCounter = 1;
+
+/**
+ * Get current list of process
+ * @returns {Array} processes array
+ */
+function getProcesses(){
+    return processes;
+}
+
+/**
+ * Add a process to the list
+ * @param {Object} process - Process object to add
+ */
+function addProcessToState(process){
+    processes.push(process);
+}
+
+/**
+ * Clear all processes
+ */
+function clearProcesses(){
+    processes = [];
+}
+
+/**
+ * Get selected algorithm
+ * @returns {string} selected algorithm key
+ */
+function getSelectedAlgorithm(){
+    return selectedAlgorithm;
+}
+
+/**
+ * Set selected algorithm
+ * @param {string} algoKey - Algorithm key to set
+ */
+function setSelectedAlgorithm(algoKey){
+    selectedAlgorithm = algoKey;
+}
+
+/**
+ * Get current process counter value
+ * @returns {number} current counter
+ */
+function getProcessCounter(){
+    return processCounter;
+}
+
+/**
+ * Increment process counter by 1
+ */
+function incrementProcessCounter(){
+    processCounter++;
+}
+
+/**
+ * Reset process counter to 1
+ */
+function resetProcessCounter(){
+    processCounter = 1;
+}
